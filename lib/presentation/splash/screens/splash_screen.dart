6eec9a8d2785ff904d83961fe304ab1cc0ca7a6a
nerufuyo <listyoadipamungkas1401@gmail.com>
2025-02-02 16:35:41 +0700

    🐞 fix: logic on splash bloc

--- lib/presentation/splash/screens/splash_screen.dart
+++ lib/presentation/splash/screens/splash_screen.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:go_router/go_router.dart';
 import 'package:uptask/core/core.dart';
+import 'package:uptask/presentation/presentation.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
+  void _navigateToNextScreen(BuildContext context) async {
+    String? token =
+        await FlutterSecureStorage().read(key: AppConstant.tokenName);
+
+    if (token != null && token.isNotEmpty) {
+      GoRouter.of(globalKey.currentContext!).go(RoutePath.home);
+    } else {
+      GoRouter.of(globalKey.currentContext!).go(RoutePath.auth);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          AppConstant.appName,
-          style: AppTypography.headline2.copyWith(
-            fontWeight: FontWeight.bold,
+    return BlocListener<SplashBloc, SplashState>(
+      listener: (context, state) {
+        if (state is SplashSuccess) _navigateToNextScreen(context);
+      },
+      child: Scaffold(
+        body: Center(
+          child: Text(
+            AppConstant.appName,
+            style: AppTypography.headline2.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
